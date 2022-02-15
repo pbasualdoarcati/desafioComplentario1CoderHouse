361c9719f2894f82091e34aea796aec227aab013
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-02-14 23:29:51 -0300

    Primer commit

---
+++ desafioComplementario.js
@@ -0,0 +1,16 @@
+//Consigna del desafio complementario: Crea un algoritmo utilizando un ciclo
+
+let numero = parseInt(prompt('Ingrese un numero'))
+
+for(let i=0; i <= numero; i++){
+    let anterior = numero-i;
+
+    // El continue lo que va a hacer el es saltear el valor que obtenga anterior si es igual a numero
+    if (anterior == numero){
+        continue
+    }
+
+    //Si ingrese 10, quiero ver, 9,8,7,6,5,4,3,2,1
+   
+    console.log(`Los anteriores a ${numero} son: ${anterior} `)
+}
